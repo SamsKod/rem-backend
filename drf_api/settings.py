269572832fbdb2c4bb35b88590a3edb000f73577
samuelwigren <samuel.wigren@gsfacket.se>
2023-10-25 18:37:10 +0200

    Update settings.py

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'schema_graph',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
